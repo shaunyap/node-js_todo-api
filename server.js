3e32160d4e7d9bed64d0e03466afaede13c210b8
Shaun Yap <shaun@siegeflow.com>
2016-05-01 16:04:24 +0400

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -24,13 +25,7 @@ app.get('/todos', function (req, res) {
 // GET todos/:id
 app.get('/todos/:id', function (req, res) {
     var todoID = parseInt(req.params.id, 10);
-    var matchedTodo;
-
-    todos.forEach(function (todo) {
-      if (todoID === todo.id) {
-        matchedTodo = todo;
-      }
-    });
+    var matchedTodo = _.findWhere(todos, todoID);
 
     if (matchedTodo) {
       res.json(matchedTodo);
@@ -41,10 +36,16 @@ app.get('/todos/:id', function (req, res) {
 
 // POST /todos
 app.post('/todos', function(req,res) {
-  var body = req.body;
+  var body = _.pick(req.body, 'description', 'completed');
+  // use _.pick to only pick description and completed
+
+  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+    return res.status(400).send();
+  }
+
+  body.description = body.description.trim();
   body.id = todoNextId;
   todoNextId++;
-//push body into array
   todos.push(body);
 
   console.log('Task added: ' + body.description);
