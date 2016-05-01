fcbd2c250b15bffc45d660ab55b3f8020c1aedbe
Shaun Yap <shaun@siegeflow.com>
2016-05-01 21:55:12 +0400

    Added ability to search using queries

--- server.js
+++ server.js
@@ -16,16 +16,21 @@ app.get('/', function(req, res) {
 });
 
 
-// GET /todos
+// GET /todos ?completed=true&q=work
 app.get('/todos', function (req, res) {
   var queryParams = req.query;
   var filteredTodos = todos;
 
-  // if has property && completed === 'true'
   if( queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
     filteredTodos = _.where(filteredTodos, {completed: true});
   } else if( queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
     filteredTodos = _.where(filteredTodos, {completed: false});
+  }
+
+  if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+    filteredTodos = _.filter(filteredTodos, function (todo) {
+      return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+    });
   }
   res.json(filteredTodos);
 });
