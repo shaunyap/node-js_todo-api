775d27a2a0028bc90cc2beb612973686cb2f35ff
Shaun Yap <shaun@siegeflow.com>
2016-05-01 11:16:33 +0400

    Added POST for todos

--- server.js
+++ server.js
@@ -1,29 +1,27 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-  id: 1,
-  description: 'meet Bob for lunch',
-  completed: false
-}, {
-  id: 2,
-  description: 'go to the market',
-    completed: false
-}, {
-  id: 3,
-  description: 'clean the house',
-    completed: false
-}
-];
 
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
+
+//GET Index
 app.get('/', function(req, res) {
   res.send('Todo Api Route.');
 });
 
+
+// GET /todos
 app.get('/todos', function (req, res) {
   res.json(todos);
 });
 
+
+// GET todos/:id
 app.get('/todos/:id', function (req, res) {
     var todoID = parseInt(req.params.id, 10);
     var matchedTodo;
@@ -39,6 +37,19 @@ app.get('/todos/:id', function (req, res) {
     } else {
       res.status(404).send();
     }
+});
+
+// POST /todos
+app.post('/todos', function(req,res) {
+  var body = req.body;
+  body.id = todoNextId;
+  todoNextId++;
+//push body into array
+  todos.push(body);
+
+  console.log('Task added: ' + body.description);
+  res.json(body);
+
 });
 
 
