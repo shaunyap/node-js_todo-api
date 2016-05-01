2d7ef5004e21689035a9daed1b85cfbf78f93a1f
Shaun Yap <shaun@siegeflow.com>
2016-05-01 17:46:59 +0400

    Added ?completed filter

--- server.js
+++ server.js
@@ -18,7 +18,16 @@ app.get('/', function(req, res) {
 
 // GET /todos
 app.get('/todos', function (req, res) {
-  res.json(todos);
+  var queryParams = req.query;
+  var filteredTodos = todos;
+
+  // if has property && completed === 'true'
+  if( queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+    filteredTodos = _.where(filteredTodos, {completed: true});
+  } else if( queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+    filteredTodos = _.where(filteredTodos, {completed: false});
+  }
+  res.json(filteredTodos);
 });
 
 
