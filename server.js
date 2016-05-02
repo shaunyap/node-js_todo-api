4553b25905e01bba57f1872d94d580f165813458
Shaun Yap <shaun@siegeflow.com>
2016-05-02 22:17:55 +0400

    Refactored GET /todos/:id to use sequelize.

--- server.js
+++ server.js
@@ -46,15 +46,15 @@ app.get('/todos', function(req, res) {
 // GET todos/:id
 app.get('/todos/:id', function(req, res) {
   var todoID = parseInt(req.params.id, 10);
-  var matchedTodo = _.findWhere(todos, {
-    id: todoID
-  });
-
-  if (matchedTodo) {
-    res.json(matchedTodo);
-  } else {
-    res.status(404).send();
-  }
+  db.todo.findById(todoID).then(function(todo) {
+    if (!!todo) {
+      res.json(todo.toJSON());
+    } else {
+      res.status(404).send();
+    }
+  }, function(e) {
+    res.status(500).send();
+  })
 });
 
 // POST /todos
@@ -66,19 +66,6 @@ app.post('/todos', function(req, res) {
   }, function(e) {
     res.status(400).json(e)
   });
-
-  // if (!_.isBoolean(body.completed) || !_.isString(body.description) || body
-  //   .description.trim().length === 0) {
-  //   return res.status(400).send();
-  // }
-  //
-  // body.description = body.description.trim();
-  // body.id = todoNextId;
-  // todoNextId++;
-  // todos.push(body);
-  //
-  // console.log('Task added: ' + body.description);
-  // res.json(body);
 });
 
 // DETELE /todos/:id
