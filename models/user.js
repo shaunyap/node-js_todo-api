39fc724d3ca7ef50d01804d0814a27093a8f7114
Shaun Yap <shaun@siegeflow.com>
2016-05-05 11:06:24 +0400

    hash and salt password

--- models/user.js
+++ models/user.js
@@ -1,23 +1,52 @@
-module.exports = function (sequelize, DataTypes) {
+var bcrypt = require('bcryptjs');
+var _ = require('underscore');
+module.exports = function(sequelize, DataTypes) {
     return sequelize.define('user', {
-      email: {
+        email: {
             type: DataTypes.STRING,
             allowNull: false,
             unique: true,
-            validate: { isEmail: true }
+            validate: {
+                isEmail: true
+            }
+        },
+        salt: {
+            type: DataTypes.STRING
+        },
+        password_hash: {
+            type: DataTypes.STRING
         },
         password: {
-            type: DataTypes.STRING,
+            type: DataTypes.VIRTUAL,
             allowNull: false,
-            validate: { len: [7, 100] }
+            validate: {
+                len: [7, 100]
+            },
+            set: function(value) {
+                var salt = bcrypt.genSaltSync(10);
+                var hashedPassword = bcrypt.hashSync(value,
+                    salt);
+
+                this.setDataValue('password', value);
+                this.setDataValue('salt', salt);
+                this.setDataValue('password_hash',
+                    hashedPassword);
+            }
         }
     }, {
         hooks: {
-            beforeValidate: function (user, options) {
-                    if (typeOf(user.email) === 'string') {
-                        user.email.toLowerCase();
-                    }
+            beforeValidate: function(user, options) {
+                if (typeof user.email === 'string') {
+                    user.email.toLowerCase();
                 }
             }
-        });
+        },
+        instanceMethods: {
+            toPublicJSON: function() {
+                var json = this.toJSON();
+                return _.pick(json, 'id', 'email',
+                    'createdAt', 'updatedAt');
+            }
+        }
+    });
 };
