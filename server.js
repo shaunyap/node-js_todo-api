2281ba34fc15606348b0724fcb74d72a03de0988
Shaun Yap <shaun@siegeflow.com>
2016-05-06 08:28:35 +0400

    Added auth middleware and findByToken class method

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
 var bcrypt = require('bcryptjs');
+var middleware = require('./middleware.js')(db);
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -18,7 +19,7 @@ app.get('/', function(req, res) {
 
 
 // GET /todos ?completed=true&q=work
-app.get('/todos', function(req, res) {
+app.get('/todos', middleware.requireAuthentication, function(req, res) {
     var query = req.query;
     var where = {};
 
@@ -47,7 +48,7 @@ app.get('/todos', function(req, res) {
 
 
 // GET todos/:id
-app.get('/todos/:id', function(req, res) {
+app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
     var todoID = parseInt(req.params.id, 10);
     db.todo.findById(todoID).then(function(todo) {
         if (!!todo) {
@@ -61,7 +62,7 @@ app.get('/todos/:id', function(req, res) {
 });
 
 // POST /todos
-app.post('/todos', function(req, res) {
+app.post('/todos', middleware.requireAuthentication, function(req, res) {
     var body = _.pick(req.body, 'description', 'completed');
     db.todo.create(body).then(function(todo) {
         res.json(todo.toJSON());
@@ -71,7 +72,7 @@ app.post('/todos', function(req, res) {
 });
 
 // DETELE /todos/:id
-app.delete('/todos/:id', function(req, res) {
+app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
     var todoID = parseInt(req.params.id, 10);
     db.todo.destroy({
         where: {
@@ -92,7 +93,7 @@ app.delete('/todos/:id', function(req, res) {
 });
 
 // PUT /todos/:id
-app.put('/todos/:id', function(req, res) {
+app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
     var todoId = parseInt(req.params.id, 10);
 
     var body = _.pick(req.body, 'description', 'completed');
