e89a207d29e073d27f65568da2fda3cd77ee8157
Shaun Yap <shaunyap@Shauns-MacBook-Pro.local>
2016-05-06 15:07:21 +0400

    Added playground for associations

---
+++ playground/basic-sqlite-database.js
@@ -0,0 +1,55 @@
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize(undefined, undefined, undefined, {
+  'dialect': 'sqlite',
+  'storage': 'basic-sqlite-database.sqlite'
+});
+
+var Todo = sequelize.define('todo', {
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      len: [1, 250]
+    }
+  },
+  completed: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: false
+  }
+})
+
+var User = sequelize.define('user', {
+  email: Sequelize.STRING
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
+
+sequelize.sync().then(function() {
+  // force: true
+}).then(function() {
+  console.log('Everything is synced.');
+
+  User.findById(1).then(function (user) {
+    user.getTodos().then(function (todos) {
+      todos.forEach(function (todo) {
+        console.log(todo.toJSON());
+      });
+    });
+  });
+
+  // User.create({
+  //   email: 'shaun@example.com'
+  // }).then(function () {
+  //   return Todo.create({
+  //     description: 'Clean Yard'
+  //   });
+  // }).then(function (todo) {
+  //   User.findById(1).then(function (user) {
+  //     user.addTodo(todo);
+  //   });
+  // });
+
+
+})
